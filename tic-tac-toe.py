f31e3af8423758fd846fff0096cfbf416b96e232
Clayton Vanfleet <dvanfleet.clayton@gmail.com>
2022-04-26 20:19:23 -0600

    This if for sure finished, still could be prettier

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -52,54 +52,61 @@ def play():
 def calculateWin(turn):
     
     if (grid[1] == turn) and (grid[2] == turn) and (grid[3] == turn):
-        print(f"The {turn}'s win!!")
-        again = input('Do you want to play again? (y/n) ')
+        again = t.textinput(f"The {turn}'s win!!",'Do you want to play again? (y/n) ')
         if again == 'y':
             t.clear()
             main()
         print('Thanks for playing! Bye!')
     elif (grid[1] == turn) and (grid[4] == turn) and (grid[7] == turn):
-        print(f"The {turn}'s win!!")
-        again = input('Do you want to play again? (y/n) ')
+        again = t.textinput(f"The {turn}'s win!!",'Do you want to play again? (y/n) ')
         if again == 'y':
             t.clear()
             main()
         print('Thanks for playing! Bye!')
     elif (grid[1] == turn) and (grid[5] == turn) and (grid[9] == turn):
-        print(f"The {turn}'s win!!")
-        again = input('Do you want to play again? (y/n) ')
+        again = t.textinput(f"The {turn}'s win!!",'Do you want to play again? (y/n) ')
         if again == 'y':
             t.clear()
             main()
         print('Thanks for playing! Bye!')
     elif (grid[2] == turn) and (grid[5] == turn) and (grid[8] == turn):
-        print(f"The {turn}'s win!!")
-        again = input('Do you want to play again? (y/n) ')
+        again = t.textinput(f"The {turn}'s win!!",'Do you want to play again? (y/n) ')
         if again == 'y':
             t.clear()
             main()
         print('Thanks for playing! Bye!')
     elif (grid[3] == turn) and (grid[5] == turn) and (grid[7] == turn):
-        print(f"The {turn}'s win!!")
-        again = input('Do you want to play again? (y/n) ')
+        again = t.textinput(f"The {turn}'s win!!",'Do you want to play again? (y/n) ')
+        if again == 'y':
+            t.clear()
+            main()
+        print('Thanks for playing! Bye!')
+    elif (grid[4] == turn) and (grid[5] == turn) and (grid[6] == turn):
+        again = t.textinput(f"The {turn}'s win!!",'Do you want to play again? (y/n) ')
+        if again == 'y':
+            t.clear()
+            main()
+        print('Thanks for playing! Bye!')
+    elif (grid[7] == turn) and (grid[8] == turn) and (grid[9] == turn):
+        again = t.textinput(f"The {turn}'s win!!",'Do you want to play again? (y/n) ')
         if again == 'y':
             t.clear()
             main()
         print('Thanks for playing! Bye!')
     elif (grid[3] == turn) and (grid[6] == turn) and (grid[9] == turn):
-        print(f"The {turn}'s win!!")
-        again = input('Do you want to play again? (y/n) ')
+        again = t.textinput(f"The {turn}'s win!!",'Do you want to play again? (y/n) ')
         if again == 'y':
             t.clear()
             main()
         print('Thanks for playing! Bye!')
+    elif None not in (grid[1],grid[2],grid[3],grid[4],grid[5],grid[6],grid[7],grid[8],grid[9]):
+        again = t.textinput("Its a draw!", "Do you want to play again? (y,n) ")
+        if again == 'y':
+            t.clear()
+            main()
     else:
         pass
 
-def fivebyfive(t,scrn):
-    print('This is five by five')
-def sixbysix(t,scrn):
-    print('This is six by six')
 
 def o(x,y):
     
@@ -107,6 +114,7 @@ def o(x,y):
     t.goto(x,y-25)
     t.down()
     t.circle(25)
+    t.update() 
 
 def ex(x,y):
     t.color('red')
@@ -122,7 +130,7 @@ def ex(x,y):
     t.down()
     t.forward(50)
 
-                                #reset ts direction
+                                #reset turtles direction
     t.right(45)
     t.left(45)
     t.left(45)
@@ -130,109 +138,145 @@ def ex(x,y):
 def getCoord(x,y):
     
     t.up()
-    
-    
+
     global turn
     
+
     if (x < -50) and (y > 50):
-        if turn == 'x':
-            grid[1] = turn
-            ex(x,y)
-            calculateWin(turn)
-            turn = 'o'
-        elif turn == 'o':
-            grid[1] = turn
-            o(x,y)
-            calculateWin(turn)
-            turn = 'x'
+        if grid[1] == None:
+            if turn == 'x':
+                grid[1] = turn
+                ex(x,y)
+                calculateWin(turn)
+                turn = 'o'
+            elif turn == 'o':
+                grid[1] = turn
+                o(x,y)
+                calculateWin(turn)
+                turn = 'x'
+        else:
+            print('Please choose an empty slot.')
+            
     elif (50 > x > -50) and (y > 50):
-        if turn == 'x':
-            grid[2] = turn
-            ex(x,y)
-            calculateWin(turn)
-            turn = 'o'
-        elif turn == 'o':
-            grid[2] = turn
-            o(x,y)
-            calculateWin(turn)
-            turn = 'x'
+        if grid[2] == None:
+            if turn == 'x':
+                grid[2] = turn
+                ex(x,y)
+                calculateWin(turn)
+                turn = 'o'
+            elif turn == 'o':
+                grid[2] = turn
+                o(x,y)
+                calculateWin(turn)
+                turn = 'x'
+        else:
+            print('Please choose an empty slot.')
+            
     elif (x>50) and (y>50):
-        if turn == 'x':
-            grid[3] = turn
-            ex(x,y)
-            calculateWin(turn)
-            turn = 'o'
-        elif turn == 'o':
-            grid[3] = turn
-            o(x,y)
-            calculateWin(turn)
-            turn = 'x'
+        if grid[3] == None:
+            if turn == 'x':
+                grid[3] = turn
+                ex(x,y)
+                calculateWin(turn)
+                turn = 'o'
+            elif turn == 'o':
+                grid[3] = turn
+                o(x,y)
+                calculateWin(turn)
+                turn = 'x'
+        else:
+            print('Please choose an empty slot.')
+
     elif (x < -50) and (50 > y > -50):
-        if turn == 'x':
-            grid[4] = turn
-            ex(x,y)
-            calculateWin(turn)
-            turn = 'o'
-        elif turn == 'o':
-            grid[4] = turn
-            o(x,y)
-            calculateWin(turn)
-            turn = 'x'
+        if grid[4] == None:
+            if turn == 'x':
+                grid[4] = turn
+                ex(x,y)
+                calculateWin(turn)
+                turn = 'o'
+            elif turn == 'o':
+                grid[4] = turn
+                o(x,y)
+                calculateWin(turn)
+                turn = 'x'
+        else:
+            print('Please choose an empty slot.')
+    
     elif (-50 < x < 50) and (50 > y > -50):
-        if turn == 'x':
-            grid[5] = turn
-            ex(x,y)
-            calculateWin(turn)
-            turn = 'o'
-        elif turn == 'o':
-            grid[5] = turn
-            o(x,y)
-            calculateWin(turn)
-            turn = 'x'
+        if grid[5] == None:
+            if turn == 'x':
+                grid[5] = turn
+                ex(x,y)
+                calculateWin(turn)
+                turn = 'o'
+            elif turn == 'o':
+                grid[5] = turn
+                o(x,y)
+                calculateWin(turn)
+                turn = 'x'
+        else: 
+            print('Please choose an empty slot.')
+            
     elif (x > 50) and (50 > y > -50):
-        if turn == 'x':
-            grid[6] = turn
-            ex(x,y)
-            calculateWin(turn)
-            turn = 'o'
-        elif turn == 'o':
-            grid[6] = turn
-            o(x,y)
-            calculateWin(turn)
-            turn = 'x'
+        if grid[6] == None:
+            if turn == 'x':
+                grid[6] = turn
+                ex(x,y)
+                calculateWin(turn)
+                turn = 'o'
+            elif turn == 'o':
+                grid[6] = turn
+                o(x,y)
+                calculateWin(turn)
+                turn = 'x'
+        else:
+            print('Please choose an empty slot.')
+            
     elif (x < -50) and (-50 > y):
-        if turn == 'x':
-            grid[7] = turn
-            ex(x,y)
-            calculateWin(turn)
-            turn = 'o'
-        elif turn == 'o':
-            grid[7] = turn
-            o(x,y)
-            calculateWin(turn)
-            turn = 'x'
+        if grid[7] == None:
+            if turn == 'x':
+                grid[7] = turn
+                ex(x,y)
+                calculateWin(turn)
+                turn = 'o'
+            elif turn == 'o':
+                grid[7] = turn
+                o(x,y)
+                calculateWin(turn)
+                turn = 'x'
+        else:
+            print('Please choose an empty slot.')
+            
     elif (50 > x > -50) and (y < -50):
-        if turn == 'x':
-            grid[8] = turn
-            ex(x,y)
-            calculateWin(turn)
-            turn = 'o'
-        elif turn == 'o':
-            grid[8] = turn
-            o(x,y)
-            calculateWin(turn)
-            turn = 'x'
+        if grid[8] == None:
+            if turn == 'x':
+                grid[8] = turn
+                ex(x,y)
+                calculateWin(turn)
+                turn = 'o'
+            elif turn == 'o':
+                grid[8] = turn
+                o(x,y)
+                calculateWin(turn)
+                turn = 'x'
+        else:
+            print('Please choose an empty slot.')
+            
     elif (x > 50) and (y < -50):
-        if turn == 'x':
-            grid[9] = turn
-            ex(x,y)
-            calculateWin(turn)
-            turn = 'o'
-        elif turn == 'o':
-            grid[9] = turn
-            o(x,y)
-            calculateWin(turn)
-            turn = 'x'
+        if grid[9] == None:
+            if turn == 'x':
+                grid[9] = turn
+                ex(x,y)
+                calculateWin(turn)
+                turn = 'o'
+            elif turn == 'o':
+                grid[9] = turn
+                o(x,y)
+                calculateWin(turn)
+                turn = 'x'
+        else:
+            print('Please choose an empty slot.')
+            
             
     
 
@@ -246,12 +290,12 @@ def main():
     grid = {1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None, 8: None, 9: None}
 
 
-    ready = input('Are you ready to play? (y/n) ')
+    ready = t.textinput('Tic-tac-toe',"Welcome to tic-tac-toe, the x's will go first! Are you ready to play? (y/n) ")
 
 
     if ready == 'y':
         play()
-    print('Okay, by!')
+    print('Okay, bye!')
        
 
 
